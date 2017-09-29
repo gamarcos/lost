a09a85414fa4497b80f960456f33569c9c5fe05b
Gabriel Marcos <gabriel.marcos@jussi.com.br>
2017-09-28 22:00:39 -0300

    Login Page

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,20 +1,19 @@
+function setHeight() {
+	var max_height = screen.height;
+	var content 			= document.querySelector(".container");
+	content.style.height = max_height;
+}
+
 document.addEventListener("DOMContentLoaded", function() {
-	
+
 	var init 				= document.querySelector(".init-view");
-	var validation_qr 		= document.querySelector(".validation-qr");
 	var validation 			= document.querySelector(".choose-validation");
-	var number_validation 	= document.querySelector(".write-code");
+	var validation_qr 		= document.querySelector(".validation-qr");
 	var validation_code 	= document.querySelector(".validation-code");
+	var number_validation 	= document.querySelector(".write-code");
 	var validation_voucher	= document.querySelector(".button-validation");
 
-    var start = document.querySelector(".button-init");
 
-	start.addEventListener("click", function(){
-
-		validation.style.display = "inherit";
-		init.style.display = "none";
-
-	});
 
 	validation_code.addEventListener("click", function() {
 		validation.style.display = "none";
@@ -28,19 +27,5 @@ document.addEventListener("DOMContentLoaded", function() {
 		var last_number = document.querySelector(".last-numbers").value
 
 		var number_voucher = first_number.concat(last_number);
-
-		fetch("")
-		.then(function(response){
-			if(response != 200){
-				return response.json()
-			}
-			throw new Error(response.status)
-		}).then(function(data) {
-			//code here
-		}).map(function(element) {
-			//code here
-		}).catch(function(err) {  
-            console.log('Fetch Error :-S', err);
-        }) 
 	})
 });
